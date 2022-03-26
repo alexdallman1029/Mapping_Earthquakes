5cbd9cf60d6a9da5ce9a8a6243732e31919a951c
AlexC <alexandria.dallman@gmail.com>
2022-03-26 09:31:27 -0700

    New Updates

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -5,14 +5,64 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Add a marker to the map for LA, CA.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
+
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location).addTo(map);
+    L.marker(city.location)
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+  radius: city.population/100000
+});
+
+
 
 //Add marker to the map for LA
-L.circleMarker([34.0522, -118.2437], {
-    radius: 100,
-    color: 'black',
-    fillColor: '#ffffa1'
-}).addTo(map);
+//L.circleMarker([34.0522, -118.2437], {
+//    radius: 100,
+//    color: 'black',
+//    fillColor: '#ffffa1'
+//}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
